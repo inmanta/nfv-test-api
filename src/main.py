4ecc6d9dd36d2db812aecb26c4cbf2e3d12cd58d
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2019-10-13 20:39:46 +0200

    Add cors

--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@ import subprocess
 import re
 import json
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Notes:
 #   * Static content available on /static
