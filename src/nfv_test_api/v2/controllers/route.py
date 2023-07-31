160299a9a8012ae7e2c27c5d2d71dd75d9821477
DariushMoh <113341735+DariushMoh@users.noreply.github.com>
2023-07-31 17:29:58 +0200

    Create, Update and Delete route added (#48)
    * Create, Update and Delete route added
    
    * version bumped to 2.4.0

--- src/nfv_test_api/v2/controllers/route.py
+++ src/nfv_test_api/v2/controllers/route.py
@@ -16,6 +16,7 @@
 from http import HTTPStatus
 from typing import Dict, Optional
 
+from flask import request  # type: ignore
 from flask_restplus import Namespace, Resource  # type: ignore
 from pydantic import ValidationError
 from werkzeug.exceptions import BadRequest  # type: ignore
@@ -23,13 +24,15 @@ from werkzeug.exceptions import BadRequest  # type: ignore
 from nfv_test_api.host import Host, NamespaceHost
 from nfv_test_api.v2.controllers.common import add_model_schema
 from nfv_test_api.v2.data.common import InputOptionalSafeName
-from nfv_test_api.v2.data.route import InputDestination, Route
+from nfv_test_api.v2.data.route import InputDestination, Route, RouteCreate, RouteUpdate
 from nfv_test_api.v2.services.namespace import NamespaceService
 from nfv_test_api.v2.services.route import RouteService
 
 namespace = Namespace(name="routes", description="Read routes on the host")
 
 route_model = add_model_schema(namespace, Route)
+route_create_model = add_model_schema(namespace, RouteCreate)
+route_update_model = add_model_schema(namespace, RouteUpdate)
 
 
 @namespace.route("")
@@ -73,6 +76,26 @@ class AllRoutes(Resource):
             raise BadRequest(str(e))
 
         return [route.json_dict() for route in self.get_service(ns_name).get_all()], HTTPStatus.OK
+
+    @namespace.expect(route_create_model)
+    @namespace.response(HTTPStatus.CREATED, "A new route has been created", route_model)
+    @namespace.response(HTTPStatus.CONFLICT, "Another route with the same name already exists")
+    @namespace.response(HTTPStatus.UNPROCESSABLE_ENTITY, "Couldn't update route because of invalid input")
+    def post(self, ns_name: Optional[str] = None):
+        """
+        Create a route on the host
+
+        The route is identified by its destination address, if another route with the same destination
+        already exists, a conflict error is raised.
+        """
+        try:
+            # Validating input
+            InputOptionalSafeName(name=ns_name)
+            create_form = RouteCreate(**request.json)
+        except ValidationError as e:
+            raise BadRequest(str(e))
+
+        return self.get_service(ns_name).create(create_form).json_dict(), HTTPStatus.CREATED
 
 
 @namespace.route("/ns/<ns_name>")
@@ -134,6 +157,43 @@ class OneRoute(Resource):
             raise BadRequest(str(e))
 
         return self.get_service(ns_name).get_one(destination).json_dict(exclude_none=True), HTTPStatus.OK
+
+    @namespace.expect(route_update_model)
+    @namespace.response(HTTPStatus.OK, "The route has been updated", route_model)
+    @namespace.response(HTTPStatus.NOT_FOUND, "Couldn't find any route with given destination address")
+    @namespace.response(HTTPStatus.UNPROCESSABLE_ENTITY, "Couldn't update route because of invalid input")
+    def patch(self, dst_addr: str, dst_prefix_len: Optional[int] = None, ns_name: Optional[str] = None):
+        """
+        Update a route on the host
+
+        The route is identified by its destination.
+        """
+        try:
+            # Validating input
+            destination = InputDestination(dst_addr=dst_addr, dst_prefix_len=dst_prefix_len).destination_name
+            InputOptionalSafeName(name=ns_name)
+            update_form = RouteUpdate(**request.json)
+        except ValidationError as e:
+            raise BadRequest(str(e))
+
+        return self.get_service(ns_name).update(destination, update_form).json_dict(exclude_none=True), HTTPStatus.OK
+
+    @namespace.response(HTTPStatus.OK, "The route doesn't exist anymore")
+    def delete(self, dst_addr: str, dst_prefix_len: Optional[int] = None, ns_name: Optional[str] = None):
+        """
+        Delete a route from the host
+
+        The route is identified by its destination. This method is idempotent, if the route
+        doesn't exist it won't try to delete it again, and consider the deletion successful.
+        """
+        try:
+            # Validating input
+            destination = InputDestination(dst_addr=dst_addr, dst_prefix_len=dst_prefix_len).destination_name
+            InputOptionalSafeName(name=ns_name)
+        except ValidationError as e:
+            raise BadRequest(str(e))
+
+        self.get_service(ns_name).delete(destination)
 
 
 @namespace.route("/<dst_addr>/<int:dst_prefix_len>")
