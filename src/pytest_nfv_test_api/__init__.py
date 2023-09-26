5279f6b897369e9b6092e52915103c8482cc4210
DariushMoh <113341735+DariushMoh@users.noreply.github.com>
2023-09-26 17:23:50 +0200

    ping timeout added (#56)

--- src/pytest_nfv_test_api/__init__.py
+++ src/pytest_nfv_test_api/__init__.py
@@ -27,7 +27,7 @@ from docker.models import containers, images  # type: ignore
 
 LOGGER = logging.getLogger(__name__)
 
-__version__ = "2.5.2"
+__version__ = "2.5.3"
 
 
 @pytest.fixture(scope="session")
